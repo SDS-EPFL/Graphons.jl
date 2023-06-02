5c3f7295efb190b8138ef8d0c69f7841931a779e
dufourc1 <dufourc115@gmail.com>
2023-06-02 14:35:06 +0200

    add common graphon function, closes #1

--- src/common_graphon.jl
+++ src/common_graphon.jl
@@ -9,3 +9,43 @@ end
 function _probs(s::GraphonFunction, i, j)
     return s.graphon_function(i, j)
 end
+
+function graphon_function_product(x, y)
+    return x * y
+end
+
+function graphon_function_exp_07(x, y)
+    return exp(-(x^0.7 + y^0.7))
+end
+
+function graphon_function_polynomial(x, y)
+    return 0.25 * (x^2 + y^2 + sqrt(x) + sqrt(y))
+end
+
+function graphon_function_mean(x, y)
+    return 0.5 * (x + y)
+end
+
+function graphon_function_logit_sum(x, y)
+    return 1 / (1 + exp(-10 * (x^2 + y^2)))
+end
+
+function graphon_function_latent_distance(x, y)
+    return abs(x - y)
+end
+
+function graphon_function_logit_max_power(x, y)
+    return 1 / (1 + exp(-(max(x, y)^2 + min(x, y)^4)))
+end
+
+function graphon_function_exp_max_power(x, y)
+    return exp(-max(x, y)^(3 / 4))
+end
+
+function graphon_function_exp_polynomial(x, y)
+    return exp(-0.5 * (min(x, y) + sqrt(x) + sqrt(y)))
+end
+
+function graphon_function_log(x, y)
+    return log1p(0.5 * max(x, y))
+end
