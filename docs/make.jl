dfc564af9ac15ceaa6e862727f81e7d524caa630
dufourc1 <dufourc115@gmail.com>
2023-06-02 13:44:08 +0200

    refactor SBM

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,22 @@
 using Graphon
 using Documenter
 
-DocMeta.setdocmeta!(Graphon, :DocTestSetup, :(using Graphon); recursive=true)
+DocMeta.setdocmeta!(Graphon, :DocTestSetup, :(using Graphon); recursive = true)
 
 makedocs(;
-    modules=[Graphon],
-    authors="Jake Grainger, Charles Dufour",
-    repo="https://github.com/JakeGrainger/Graphon.jl/blob/{commit}{path}#{line}",
-    sitename="Graphon.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://JakeGrainger.github.io/Graphon.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
+         modules = [Graphon],
+         authors = "Jake Grainger, Charles Dufour",
+         repo = "https://github.com/JakeGrainger/Graphon.jl/blob/{commit}{path}#{line}",
+         sitename = "Graphon.jl",
+         format = Documenter.HTML(;
+                                  prettyurls = get(ENV, "CI", "false") == "true",
+                                  canonical = "https://JakeGrainger.github.io/Graphon.jl",
+                                  edit_link = "main",
+                                  assets = String[]),
+         pages = [
+             "Home" => "index.md",
+         ])
 
 deploydocs(;
-    repo="github.com/JakeGrainger/Graphon.jl",
-    devbranch="main",
-)
+           repo = "github.com/JakeGrainger/Graphon.jl",
+           devbranch = "main")
--- src/Graphon.jl
+++ src/Graphon.jl
@@ -1,26 +1,52 @@
 module Graphon
 
 struct SBM
-	θ::Matrix{Float64}
-	size::Vector{Float64}
+    θ::Matrix{Float64}
+    size::Vector{Float64}
+    cumulative_size::Vector{Float64}
+end
+
+function SBM(θ::Matrix{Float64}, groupsize::Vector{Float64})
+    @assert size(θ, 1) == size(θ, 2)
+    @assert length(groupsize) == size(θ, 1)
+    @assert all(0 .<= θ .<= 1)
+    @assert all(0 .<= groupsize .<= 1)
+    @assert sum(groupsize) ≈ 1
+    return SBM(θ, groupsize, cumsum(groupsize))
 end
 
 function _rand(s::SBM, i, j)
-	return Int(rand()<s.θ[findfirst(x -> i<=x, s.size),findfirst(x -> j<=x, s.size)])
+    return Int(rand() < s.θ[findfirst(x -> i <= x, s.cumulative_size),
+                   findfirst(x -> j <= x, s.cumulative_size)])
 end
 
-function simulate(s::SBM, n)
-	A = Matrix{Int}(undef, n, n)
-	for i in 1:n
-		A[i,i] = 0
-		for j in i+1:n
-			A[i,j] = _rand(s, i/n, j/n)
-			A[j,i] = A[i,j]
-		end
-	end
-	return A
+function draw(s::SBM, n, latent)
+    A = Matrix{Int}(undef, n, n)
+    for i in 1:n
+        A[i, i] = 0
+        for j in (i + 1):n
+            A[i, j] = _rand(s, latent[i], latent[j])
+            A[j, i] = A[i, j]
+        end
+    end
+    return A
 end
 
-export SBM, simulate
+function draw_non_exchangeable(s::SBM, n)
+    A = Matrix{Int}(undef, n, n)
+    for i in 1:n
+        A[i, i] = 0
+        for j in (i + 1):n
+            A[i, j] = _rand(s, i / n, j / n)
+            A[j, i] = A[i, j]
+        end
+    end
+    return A
+end
+
+draw_exchangeable(s::SBM, n) = draw(s, n, rand(Uniform(0, 1), n))
+sample(s::SBM, n) = draw_exchangeable(s, n)
+
+export SBM, sample
 
 end
