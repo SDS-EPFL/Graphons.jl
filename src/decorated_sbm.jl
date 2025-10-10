062e3438a2db8f2fdff8dc9572f60bb5869dd7b5
dufourc1 <dufourc115@gmail.com>
2025-10-10 17:05:33 +0200

    add generation from arbitrary decorated sbm

--- src/decorated_sbm.jl
+++ src/decorated_sbm.jl
@@ -11,3 +11,20 @@ function DecoratedSBM(θ::AbstractMatrix{D}, sizes, M=Matrix{eltype(D)}) where {
     @assert last(cumsizes) ≈ 1
     return DecoratedSBM{eltype(θ),M,typeof(θ),typeof(sizes),typeof(cumsizes)}(θ, sizes, cumsizes)
 end
+
+
+
+function _rand!(rng::AbstractRNG, f::DecoratedSBM{D,M}, A::M, ξs) where {D,M}
+    latents = map(x -> findfirst(y -> x <= y, f.cumsize), ξs)
+    fill!(A, zero(eltype(A)))
+    for j in axes(A, 2)
+        for i in axes(A, 1)
+            if i <= j
+                A[i, j] = A[j, i]
+            else
+                A[i, j] = rand(rng, f.θ[latents[i], latents[j]])
+            end
+        end
+    end
+    return A
+end
